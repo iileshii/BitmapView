d47d94fd7e7824e732a2f9a17844a6d3c89d3403
Alexey Kuznetsov <kuznetsov.a.e@gmail.com>
2015-07-23 15:09:48 +0600

    v. 1.1
    Bitmap moves smoother.
    TODO: set Borders
    TODO: calculate angle to rotate
    TODO: rotate around own center | finger center | something else

--- app/src/main/java/org/iileshii/kuznetsov/alexey/bitmapview/BitmapView.java
+++ app/src/main/java/org/iileshii/kuznetsov/alexey/bitmapview/BitmapView.java
@@ -5,31 +5,28 @@ import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
-import android.graphics.Color;
 import android.graphics.Matrix;
 import android.graphics.Paint;
-import android.graphics.Point;
 import android.util.AttributeSet;
 import android.view.DragEvent;
 import android.view.MotionEvent;
 import android.view.View;
-import android.widget.FrameLayout;
 
 /**
- * @class creates CustomView
- * Created by Alexey on 22.07.2015.
+ * Bitmap view
+ * Created by Alexey on 23.07.2015.
  */
 public class BitmapView extends View {
+    // View border params
+    private static final int LEFT_VIEW_BORDER = 0;
+    private static final int TOP_VIEW_BORDER = 0;
+    private static int RIGHT_VIEW_BORDER;
+    private static int BOTTOM_VIEW_BORDER;
     private Paint paint;
     private Bitmap bitmap;
     private Matrix matrix;
-
-    private float scaleFactor = 0.4f;
-
-    private float left;
-    private float top;
-
-    private boolean moving;
+    private float[] zero = {0, 0};
+    private float[] end = {0, 0};
 
 
     public BitmapView(Context context, AttributeSet attrs) {
@@ -39,124 +36,126 @@ public class BitmapView extends View {
 
     private void init() {
         paint = new Paint();
-        setColor(Color.GREEN);
         matrix = new Matrix();
-        left = 0;
-        top = 0;
-        matrix.setScale(scaleFactor, scaleFactor);
-        moving = false;
         bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.image);
+
+        RIGHT_VIEW_BORDER = bitmap.getWidth();
+        BOTTOM_VIEW_BORDER = bitmap.getHeight();
+        end[0] = bitmap.getWidth();
+        end[1] = bitmap.getHeight();
+
         setOnDragListener(new CustomDragListener());
         setOnTouchListener(new CustomTouchListener());
     }
 
-
     public float getBitmapLeft() {
-        return left;
+        float[] dst = new float[2];
+        matrix.mapPoints(dst, zero);
+        return Math.min(dst[0], dst[1]);
     }
 
     public float getBitmapTop() {
-        return top;
+        float[] dst = new float[2];
+        matrix.mapPoints(dst, zero);
+        return Math.min(dst[0], dst[1]);
     }
 
     public float getBitmapRight() {
-        return left + bitmap.getWidth() * scaleFactor / 2;
+        float[] dst = new float[2];
+        matrix.mapPoints(dst, end);
+        return Math.max(dst[0], dst[1]);
     }
 
     public float getBitmapBottom() {
-        return top + bitmap.getHeight() * scaleFactor / 2;
+        float[] dst = new float[2];
+        matrix.mapPoints(dst, end);
+        return Math.max(dst[0], dst[1]);
     }
 
-    public void setPlace(float dX, float dY) {
-        if (left + dX < 0 || top + dY < 0 ||
-                getBitmapRight() + dX > 700 || getBitmapBottom() + dY > 1050)
-            return;
-        left = left + dX;
-        top = top + dY;
-        moving = true;
-        invalidate();
+    public float getCenterX() {
+        return getBitmapRight() + (getBitmapRight() - getBitmapLeft()) / 2;
     }
+
+    public float getCenterY() {
+        return getBitmapTop() + (getBitmapBottom() - getBitmapTop()) / 2;
+    }
+
+    public void moving(int dX, int dY) {
+        if (getBitmapLeft() + dX >= LEFT_VIEW_BORDER &&
+                getBitmapTop() + dY >= TOP_VIEW_BORDER &&
+                getBitmapRight() + dX <= RIGHT_VIEW_BORDER &&
+                getBitmapBottom() + dY <= BOTTOM_VIEW_BORDER) {
+                    matrix.postTranslate(dX, dY);
+                }
+    }
+
 
     public boolean isContent(float x, float y) {
-        return (x >= left && y >= top && x <= getBitmapRight() && y <= getBitmapRight());
-    }
-
-    public void setScale(float scaleFactor) {
-        this.scaleFactor *= Math.pow(scaleFactor, 1.0 / 5);
-        this.scaleFactor = Math.max(0.1f, Math.min(this.scaleFactor, 2.0f));
-        matrix.setScale(this.scaleFactor, this.scaleFactor);
-        invalidate();
+        return (x >= getBitmapLeft() && y >= getBitmapTop() && x <= getBitmapRight() && y <= getBitmapBottom());
     }
 
     public void setRotate(float angle) {
-        matrix.setRotate(
-                angle,
-                bitmap.getWidth() * scaleFactor / 2,
-                bitmap.getHeight() * scaleFactor / 2);
-        invalidate();
+        matrix.postRotate(angle, getCenterX(), getCenterY());
     }
 
-    public void setColor(int color) {
-        paint.setColor(color);
+    public void setScale(float scaleFactor) {
+        if (scaleFactor < 2.0f && scaleFactor > 0.4f) {
+            matrix.postScale(scaleFactor, scaleFactor);
+        }
+    }
+
+    private float getAngle(float xZero, float yZero, float xNext, float yNext) {
+        float angle = (float) Math.toDegrees(Math.atan2((yNext - yZero), (xNext - xZero)));
+
+//        if (angle < 0) {
+//            angle += 360;
+//        }
+
+        // TODO   return angle;
+        return 1;
+    }
+
+
+    @Override
+    public void onWindowFocusChanged(boolean hasWindowFocus) {
+        super.onWindowFocusChanged(hasWindowFocus);
+        RIGHT_VIEW_BORDER = getRight();
+        BOTTOM_VIEW_BORDER = getBottom();
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
         canvas.save();
-        if (moving) {
-            canvas.drawBitmap(bitmap, left, top, paint);
-            moving = false;
-        } else {
-            canvas.drawBitmap(bitmap, matrix, paint);
-            left = 0;
-            top = 0;
-        }
-
+        canvas.drawBitmap(bitmap, matrix, paint);
         canvas.restore();
     }
 
-    private float getAngle(float xZero, float yZero, float xNext, float yNext) {
-        float angle = (float) Math.toDegrees(Math.atan2((yNext - yZero), (xNext - xZero)));
-
-        if (angle < 0) {
-            angle += 360;
-        }
-
-        return angle;
-    }
-
     private class CustomDragListener implements OnDragListener {
-        float firstX, firstY;
-        FrameLayout.LayoutParams layoutParams;
+        int lastX;
+        int lastY;
 
         @Override
         public boolean onDrag(View v, DragEvent event) {
             switch (event.getAction()) {
                 case DragEvent.ACTION_DRAG_STARTED:
-                    layoutParams = (FrameLayout.LayoutParams) getLayoutParams();
                     break;
                 case DragEvent.ACTION_DRAG_ENTERED:
-                    firstX = (int) event.getX() -
-                            ((BitmapView) v).getBitmapLeft() + layoutParams.leftMargin;
-                    firstY = (int) event.getY() -
-                            ((BitmapView) v).getBitmapTop() + layoutParams.topMargin;
+                    lastX = (int) event.getX();
+                    lastY = (int) event.getY();
                     break;
                 case DragEvent.ACTION_DRAG_EXITED:
                     break;
                 case DragEvent.ACTION_DRAG_LOCATION:
-                    ((BitmapView) v).setPlace(
-                            (int) event.getX() - firstX,
-                            (int) event.getY() - firstY);
+                    ((BitmapView) v) //Draw grey rectangle follows finger
+                            .moving((int) event.getX() - lastX, (int) event.getY() - lastY);
+                    lastX = (int) event.getX();
+                    lastY = (int) event.getY();
                     invalidate();
                     break;
                 case DragEvent.ACTION_DRAG_ENDED:
                     break;
                 case DragEvent.ACTION_DROP:
-                    ((BitmapView) v).setPlace(
-                            (int) event.getX() - firstX,
-                            (int) event.getY() - firstY);
-                    invalidate();
                     break;
                 default:
                     break;
@@ -172,8 +171,6 @@ public class BitmapView extends View {
         int touchState;
         double distCurrent = 1;
         double dist0 = 1;
-        Point fingerPosition0;
-        Point fingerPosition1;
         float zeroX;
         float zeroY;
 
@@ -184,7 +181,6 @@ public class BitmapView extends View {
             switch (motionEvent.getAction() & MotionEvent.ACTION_MASK) {
                 case MotionEvent.ACTION_DOWN:
                     touchState = TOUCH;
-                    fingerPosition0 = new Point((int) motionEvent.getX(), (int) motionEvent.getY());
                     break;
                 case MotionEvent.ACTION_POINTER_DOWN:
                     touchState = PINCH;
@@ -199,30 +195,19 @@ public class BitmapView extends View {
                         distx = motionEvent.getX(0) - motionEvent.getX(1);
                         disty = motionEvent.getY(0) - motionEvent.getY(1);
                         distCurrent = Math.sqrt(distx * distx + disty * disty);
-                        if (Math.abs(distCurrent - dist0) < 50f
-                                && Math.abs(distCurrent - dist0) > 1f) {
-                            float angle = getAngle(
-                                    zeroX,
-                                    zeroY,
-                                    motionEvent.getX(0),
-                                    motionEvent.getY(0));
-                            setRotate(angle);
-                        } else {
-                            setScale((float) (distCurrent / dist0));
-                        }
-                    } else if (touchState == TOUCH
-                            && isContent(motionEvent.getX(), motionEvent.getY())) {
-                        fingerPosition1 =
-                                new Point((int) motionEvent.getX(), (int) motionEvent.getY());
-                        int dX = fingerPosition1.x - fingerPosition0.x;
-                        int dY = fingerPosition1.y - fingerPosition0.y;
-                        double distPosition = Math.sqrt(dX * dX + dY * dY);
-                        if (distPosition > 1f) {
-                            ClipData data = ClipData.newPlainText("", "");
-                            DragShadowBuilder shadowBuilder = new DragShadowBuilder();
-                            view.startDrag(data, shadowBuilder, view, 0);
-                            view.setVisibility(VISIBLE);
-                        }
+                        float angle = getAngle(
+                                zeroX,
+                                zeroY,
+                                motionEvent.getX(0),
+                                motionEvent.getY(0));
+                        setRotate(angle);
+                        setScale((float) (distCurrent / dist0));
+                        invalidate();
+                    } else if (touchState == TOUCH && isContent(motionEvent.getX(), motionEvent.getY())) {
+                        ClipData data = ClipData.newPlainText("", "");
+                        DragShadowBuilder shadowBuilder = new DragShadowBuilder();
+                        view.startDrag(data, shadowBuilder, view, 0);
+                        view.setVisibility(VISIBLE);
                     }
                     break;
                 case MotionEvent.ACTION_UP:
